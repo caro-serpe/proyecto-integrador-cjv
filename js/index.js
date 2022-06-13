381ec72d7a9ab73c13bcbcee8d185546770af6cd
JustinaVergara <vergaram@udesa.edu.ar>
2022-06-13 20:57:24 -0300

    index

--- js/index.js
+++ js/index.js
@@ -1,25 +1,26 @@
 let proxy ="https://api.allorigins.win/raw?url="
 let endpoint = "https://api.deezer.com/chart"
 
-for(let i=0; i<=4; i++){
+
 fetch(proxy+endpoint) 
     .then(function(response){
         return response.json();
     })
     .then(function(datos){
+        for(let i=0; i<=4; i++){
         console.log(datos);
-        let canciones = datos.tracks.data[i];
+        let canciones = datos.tracks.data;
         let contenedor = document.querySelector('.IndexCanciones');
 
         contenedor.innerHTML +=
         `<article> 
-        <img src= "${canciones.picture_medium}">
-        <h4> ${canciones.title}</h4>
-        <p> ${canciones.artist.name} </p>
-        <a href="./detalle-canciones.html" class="verMas"> Ver más </a>
-        </article>`
+        <img src= "${canciones[i].album.cover}">
+        <a href="./detalle-canciones.html"> <h4>${canciones[i].title}</h4></a>
+        <p> ${canciones[i].artist.name} </p>
+        <p><a class="fav" href="">Agregar a Fav</a></p>
+        </article>`}
 
     })
     .catch(function(error){
         console.log('El error fue ' + error);
-    })}
+    })
