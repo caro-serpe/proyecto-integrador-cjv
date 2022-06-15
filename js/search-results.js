d395157a416bd3174792a291f32a926199d92150
cserpe@udesa.edu.ar <cserpe@udesa.edu.ar>
2022-06-14 22:32:49 -0300

    solucione el error de cors

--- js/search-results.js
+++ js/search-results.js
@@ -1,8 +1,9 @@
 let form = document.querySelector('.form');
 let busqueda = document.querySelector('.submit-button');
 
+let hideSpinner = () => document.querySelector(".spinner").style.display = "none";
 
-let url = "https://api.deezer.com";
+let url = "https://api.allorigins.win/raw?url=https://api.deezer.com";
 
 let queryStringObj = new URLSearchParams(window.location.search);
 
@@ -12,15 +13,22 @@ let endpoint = queryStringObj.get("endpoint");
 
 // declaro todas las funciones para pegarle a la API
 let fetchPlaylist = (searchTerm) => {
-    fetch(url + "/playlist/" + searchTerm).then(response => {
-        console.log(response);
+    fetch(url + "/playlist/" + searchTerm).then(response => response.json()).then(async response => {
+        // una vez que tengo la rta, oculto el spinner
+        // await new Promise(r => setTimeout(r, 2000));
+        hideSpinner();
+        console.log(response); 
         const responseContainerElement = document.querySelector(".response-container");
         // una vez que tengo la rta, lleno el contenedor con la data como yo quiera
+
+        
     });
 };
 
 let fetchSong = (searchTerm) => {
-    fetch(url + "/track/" + searchTerm).then(response => {
+    fetch(url + "/track/" + searchTerm).then(response => response.json()).then(response => {
+        // una vez que tengo la rta, oculto el spinner
+        hideSpinner();
         console.log(response);
         const responseContainerElement = document.querySelector(".response-container");
         // una vez que tengo la rta, lleno el contenedor con la data como yo quiera
@@ -35,4 +43,3 @@ if(endpoint === "/playlist") {
 } else if(endpoint === "/song") {
     fetchSong(searchTerm);
 }
-
