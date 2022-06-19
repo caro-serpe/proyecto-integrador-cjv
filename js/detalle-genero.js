533d5efd5735c5ed66f20c876c9b89f0c9a5c1d3
Valentina Nair Kolek <valentina@AirdeValentina.fibertel.com.ar>
2022-06-19 17:02:47 -0300

    detalle de genero

--- js/detalle-genero.js
+++ js/detalle-genero.js
@@ -1,13 +1,22 @@
-let url = 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre';
+let proxy ="https://cors-anywhere.herokuapp.com/"
+let endpoint = "https://api.deezer.com/chart"
 
-fetch(url)
+fetch(proxy+endpoint)
     .then(function(response){
-    return response.json()
+    return response.json();
     })
     .then(function(datos){
         for(let i=0; i<11; i++){
             console.log(datos);
             let genero = datos.data; 
+            let contenedor = documento.querySelector('.artistGenero');
+
+            contenedor.innerHTML +=
+            `<article> 
+            <img src= "${canciones[i].album.cover_xl}">
+            <a href="./detalle-canciones.html?id=${canciones[i].id}"> <h4>${canciones[i].title}</h4></a>
+            <a href= "./detail.artist.html?id=${canciones[i].artist.id}"> ${canciones[i].artist.name} </a>
+            </article>`
         }
 
     })
