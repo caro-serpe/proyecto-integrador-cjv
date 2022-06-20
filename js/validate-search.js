1e9ab8e0d211c04de59167d5a98b1183c0283e8a
cserpe@udesa.edu.ar <cserpe@udesa.edu.ar>
2022-06-20 20:47:52 -0300

    saqué return de validate-search

--- js/validate-search.js
+++ js/validate-search.js
@@ -5,22 +5,18 @@ form.addEventListener('click',function() {
     if(form === undefined){
         alert("El campo está vacío");
         e.preventDefault();
-        return;
     }else if(form.length < 3){
         alert("El término buscado debe tener al menos 3 caracteres.")
         e.preventDefault();
-        return;
     }
 });
 busqueda.addEventListener('submit', function(){
     if(form === undefined){
         alert("El campo está vacío");
         e.preventDefault();
-        return;
     }else if(form.length < 3){
         alert("El término buscado debe tener al menos 3 caracteres.")
         e.preventDefault();
-        return;
     }
 });
 
